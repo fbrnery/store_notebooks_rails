b430d6c99bcc0d26e9dd0a5db2a252e23e53e301
Fabricio Miranda <fbrnery@gmail.com>
2022-07-22 09:28:59 -0300

    Atualizamos um layout de todo o aplicativo, bem como uma visualização específica do controlador para mostrar uma lista de produtos.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+Product.create!(title: 'Notebook Inspiron 15 3000',
+  description:
+    %{<p>
+    Processador Intel® Pentium® Gold 7505 (cache de 4MB, até 3.5GHz) Sistema operacional 
+    (A Dell Technologies recomenda o Windows 11 Pro para empresas) Windows 11 Home Single Language, 
+    Português Placa de vídeo Placa de vídeo Intel® UHD com memória gráfica compartilhada Tela Full HD
+     de 15.6" (1920 x 1080) WVA Memória Memória de 4GB (1x4GB), DDR4, 2666MHz; Expansível até 16GB 
+     (2 slots soDIMM) Armazenamento SSD de 128GB PCIe NVMe M.2 Cor Apollo - Preto Carbono Microsoft 
+     Office Sem Microsoft Office - Avaliação de 30 dias Software de Segurança McAfee® LiveSafe™ - 
+     1 ano Assistência técnica 1 ano de garantia básica via correios Complete Care Sem Complete. 
+      </p>},
+  image_url: '2999.0',
+  price: 38.00)
+# . . .
